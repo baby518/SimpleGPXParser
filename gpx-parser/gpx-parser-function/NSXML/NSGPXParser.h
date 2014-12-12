dd2b7e373380eb26e023fcdae29f6ee958d9dad7
givemeakiss518 <jerryzhang518@gmail.com>
2014-12-12 22:36:00 +0800

    以文件夹形式整理文件； 添加NSGPXParser

---
+++ gpx-parser/gpx-parser-function/NSXML/NSGPXParser.h
@@ -0,0 +1,13 @@
+//
+//  NSObject+NSGPXParser.h
+//  gpx-parser
+//
+//  Created by zhangchao on 14/12/12.
+//  Copyright (c) 2014年 zhangchao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSGPXParser:NSObject
+
+@end
